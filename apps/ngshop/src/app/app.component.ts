fa13a09c36981eb941c740ef9d21c4f0bc75674f
jbielt <jbielturon@gmail.com>
2021-07-19 17:28:44 +0200

    structuring shared styles

--- apps/ngshop/src/app/app.component.ts
+++ apps/ngshop/src/app/app.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'eastblue-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  selector: 'ngshop-root',
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'ngshop';
