94c2dd449b4c8dc9f2096fdfc89cd3b46f6a70dc
jbielt <jbielturon@gmail.com>
2021-08-14 06:46:14 +0200

    logout deleting token

--- libs/users/src/lib/services/auth.service.ts
+++ libs/users/src/lib/services/auth.service.ts
@@ -3,6 +3,8 @@ import {environment} from "@env/environment";
 import {Observable} from "rxjs";
 import {User} from "@eastblue/users";
 import {HttpClient} from "@angular/common/http";
+import {LocalstorageService} from "./localstorage.service";
+import {Router} from "@angular/router";
 
 @Injectable({
   providedIn: 'root'
@@ -11,9 +13,16 @@ export class AuthService {
   apiURLUsers = environment.apiURL + 'users';
 
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,
+              private localStorageToken: LocalstorageService,
+              private router: Router) { }
 
   login(email: string, password: string): Observable<User> {
     return this.http.post<User>(`${this.apiURLUsers}/login`, {email, password});
+  }
+
+  logout() {
+    this.localStorageToken.removeToken();
+    this.router.navigate(['/login']);
   }
 }
