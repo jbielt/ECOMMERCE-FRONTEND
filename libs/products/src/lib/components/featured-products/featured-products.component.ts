653ee8747bde5726e398c75bcbb1a6888e9652a8
jbielt <jbielturon@gmail.com>
2021-08-17 07:45:44 +0200

    calling featuredProducts on template with input

--- libs/products/src/lib/components/featured-products/featured-products.component.ts
+++ libs/products/src/lib/components/featured-products/featured-products.component.ts
@@ -1,14 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Product} from "@eastblue/products";
+import {ProductsService} from '../../services/products.service';
+import {Subject} from "rxjs";
+import {takeUntil} from "rxjs/operators";
+
 
 @Component({
   selector: 'products-featured-products',
   templateUrl: './featured-products.component.html'
 })
-export class FeaturedProductsComponent implements OnInit {
+export class FeaturedProductsComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  featuredProducts: Product[] = [];
+  endSubscription$: Subject<any> = new Subject();
+
+  constructor(private productService: ProductsService) { }
 
   ngOnInit(): void {
+    this._getFeaturedProducts();
   }
+
+  ngOnDestroy() {
+    this.endSubscription$.next();
+    this.endSubscription$.complete();
+  }
+
+  private _getFeaturedProducts() {
+    this.productService
+      .getFeaturedProducts(4)
+      .pipe(takeUntil(this.endSubscription$))
+      .subscribe((products) => {
+        this.featuredProducts = products;
+        console.log(this.featuredProducts)
+      })
+  }
+
 
 }
