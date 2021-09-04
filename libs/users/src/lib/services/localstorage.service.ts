07d4b23a66723aac614423f1ca4b4b9913ba91ed
jbielt <jbielturon@gmail.com>
2021-09-04 08:21:38 +0200

    created buildUserSession$ effect and added getUserIdFromToken on localStorageService

--- libs/users/src/lib/services/localstorage.service.ts
+++ libs/users/src/lib/services/localstorage.service.ts
@@ -31,6 +31,20 @@ export class LocalstorageService {
     }
   }
 
+  getUserIdFromToken() {
+    const token = this.getToken();
+    if(token) {
+      const tokenDecode = JSON.parse(atob(token.split('.')[1]));
+      if(tokenDecode) {
+        return tokenDecode.userId;
+      } else {
+        return null;
+      }
+    } else {
+      return null;
+    }
+  }
+
   private _tokenExpired(expiration: any): boolean {
     return Math.floor(new Date().getTime() / 1000) >= expiration;
   }
--- libs/users/src/lib/state/users.effects.ts
+++ libs/users/src/lib/state/users.effects.ts
@@ -5,21 +5,37 @@ import { fetch } from '@nrwl/angular';
 import * as UsersActions from './users.actions';
 import * as UsersFeature from './users.reducer';
 import {LocalstorageService} from "../services/localstorage.service";
+import {of} from "rxjs";
+import {catchError, concatMap, map} from "rxjs/operators";
+import {UsersService} from "@eastblue/users";
 
 @Injectable()
 export class UsersEffects {
 
-  buildUserSession$ = createEffect(() => this.actions$.pipe(
-    ofType(UsersActions.buildUserSession),
-    concatMap(() => {
-      if(this.localstorageService.isValidToken()) {
-
-      } else {
-
-      }
-    })
-  ))
+  buildUserSession$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(UsersActions.buildUserSession),
+      concatMap(() => {
+        if(this.localstorageService.isValidToken()) {
+          const userId = this.localstorageService.getUserIdFromToken();
+          if(userId) {
+            return this.usersService.getUser(userId).pipe(
+              map((user) => {
+                return UsersActions.buildUserSessionSuccess({user: user});
+              }),
+              catchError(() => of(UsersActions.buildUserSessionFailed()))
+            );
+          } else {
+            return of(UsersActions.buildUserSessionFailed());
+          }
+        } else {
+          return of(UsersActions.buildUserSessionFailed());
+        }
+      })
+    )
+  )
 
   constructor(private readonly actions$: Actions,
-              private localstorageService: LocalstorageService) {}
+              private localstorageService: LocalstorageService,
+              private usersService: UsersService) {}
 }
