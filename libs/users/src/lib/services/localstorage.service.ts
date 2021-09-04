8794c5c4fec634a08ecfd21ec94ce56a15401ffe
jbielt <jbielturon@gmail.com>
2021-09-04 08:03:41 +0200

    created validToken on localStorageService and creating effect buildUserSession in process

--- libs/users/src/lib/services/localstorage.service.ts
+++ libs/users/src/lib/services/localstorage.service.ts
@@ -20,4 +20,19 @@ export class LocalstorageService {
   removeToken()  {
     localStorage.removeItem(TOKEN);
   }
+
+  isValidToken() {
+    const token = this.getToken();
+    if(token) {
+      const tokenDecode = JSON.parse(atob(token.split('.')[1]));
+      return !this._tokenExpired(tokenDecode.exp);
+    } else {
+      return false;
+    }
+  }
+
+  private _tokenExpired(expiration: any): boolean {
+    return Math.floor(new Date().getTime() / 1000) >= expiration;
+  }
+
 }
--- libs/users/src/lib/state/users.effects.ts
+++ libs/users/src/lib/state/users.effects.ts
@@ -4,24 +4,22 @@ import { fetch } from '@nrwl/angular';
 
 import * as UsersActions from './users.actions';
 import * as UsersFeature from './users.reducer';
+import {LocalstorageService} from "../services/localstorage.service";
 
 @Injectable()
 export class UsersEffects {
-  init$ = createEffect(() =>
-    this.actions$.pipe(
-      ofType(UsersActions.init),
-      fetch({
-        run: (action) => {
-          // Your custom service 'load' logic goes here. For now just return a success action...
-          return UsersActions.loadUsersSuccess({ users: [] });
-        },
-        onError: (action, error) => {
-          console.error('Error', error);
-          return UsersActions.loadUsersFailure({ error });
-        },
-      })
-    )
-  );
 
-  constructor(private readonly actions$: Actions) {}
+  buildUserSession$ = createEffect(() => this.actions$.pipe(
+    ofType(UsersActions.buildUserSession),
+    concatMap(() => {
+      if(this.localstorageService.isValidToken()) {
+
+      } else {
+
+      }
+    })
+  ))
+
+  constructor(private readonly actions$: Actions,
+              private localstorageService: LocalstorageService) {}
 }
