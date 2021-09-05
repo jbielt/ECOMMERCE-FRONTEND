14966a6025da94283d207c1af7a99007d34d0264
jbielt <jbielturon@gmail.com>
2021-09-05 08:14:37 +0200

    updating CORE package.json and migrations.json

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
+const { getJestProjects } = require('@nrwl/jest');
+
 module.exports = {
   projects: [
+    ...getJestProjects(),
     '<rootDir>/apps/ngshop',
     '<rootDir>/apps/admin',
     '<rootDir>/libs/ui',
